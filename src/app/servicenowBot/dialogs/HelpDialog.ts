f09a7a349f948cd63451de00cc488f86dd3b17cf
Marcos Zanré <marcosz@microsoft.com>
2020-04-21 15:33:57 -0300

    sdasdasdd

--- src/app/servicenowBot/dialogs/HelpDialog.ts
+++ src/app/servicenowBot/dialogs/HelpDialog.ts
@@ -1,5 +1,5 @@
 import { Dialog, DialogContext, DialogTurnResult, ComponentDialog, WaterfallDialog, WaterfallStepContext, TextPrompt, DialogSet, DialogTurnStatus } from "botbuilder-dialogs";
-import { CardFactory, AttachmentLayoutTypes } from "botbuilder";
+import { CardFactory, AttachmentLayoutTypes, Attachment } from "botbuilder";
 const axios = require('axios');
 
 export default class HelpDialog extends ComponentDialog {
@@ -9,7 +9,7 @@ export default class HelpDialog extends ComponentDialog {
 
     public async beginDialog(context: DialogContext, options?: any): Promise<DialogTurnResult> {
 
-        let msg = CardFactory.adaptiveCard(
+        let msg: Attachment = 
             {
                 "contentType": "application/vnd.microsoft.teams.card.list",
                 "content": {
@@ -58,8 +58,8 @@ export default class HelpDialog extends ComponentDialog {
                     }
                   ]
                 }
-              }
-        );
+        }
+        
         await context.context.sendActivity({ attachments: [msg] });
         
         // context.context.sendActivity(`I'm just a friendly but rather stupid bot, and right now I don't have any valuable help for you!`); 
